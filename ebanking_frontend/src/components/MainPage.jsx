69d8e9a3cad8c75563fa30630604bd360666e621
StefanMastilovicFaks <stefan.mastilovic@tutanota.com>
2023-09-05 06:54:30 +0200

    Dodat javni API za dobijanje finansijskih vesti

--- ebanking_frontend/src/components/MainPage.jsx
+++ ebanking_frontend/src/components/MainPage.jsx
@@ -1,7 +1,26 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import './MainPage.css';
+import axios from 'axios';
 
 const MainPage = () => {
+
+  const [news, setNews] = useState('');
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        let apiUrl = 'https://eodhistoricaldata.com/api/news?api_token=demo&s=AAPL.US&offset=0&limit=10';
+
+        const response = await axios.get(apiUrl);
+        setNews(response.data[0].content);
+        // console.log(response.data[0].content);
+      } catch (error) {
+        console.error('Fetching questions failed:', error);
+      }
+    };
+    fetchData();
+  }, [])
+
   return (
     <div className="main-page">
       {/* <div> */}
@@ -9,6 +28,10 @@ const MainPage = () => {
       {/* </div> */}
       <p>
         Drago nam je što ste izabrali našu banku za vaše finansijske potrebe. Mi smo pouzdana finansijska institucija koja pruža širok spektar usluga i proizvoda kako biste mogli da ostvarite svoje ciljeve i želje.
+      </p>
+      <h1>Berza i finansijske vesti:</h1>
+      <p>
+        {news}
       </p>
     </div>
   );
